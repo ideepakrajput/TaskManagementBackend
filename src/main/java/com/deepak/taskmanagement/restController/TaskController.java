3e99ee535b9fa8a6da6c2d9dd9b021df25368d35
ideepakrajput <deepakkumar18112001@gmail.com>
2024-01-09 13:08:17 +0530

    Delete Fixed

--- src/main/java/com/deepak/taskmanagement/restController/TaskController.java
+++ src/main/java/com/deepak/taskmanagement/restController/TaskController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -61,7 +60,7 @@ public class TaskController {
 		return new ResponseEntity<>(response, HttpStatus.OK);
 	}
 
-	@DeleteMapping("deleteTask")
+	@PostMapping("deleteTask")
 	public ResponseEntity<BasicResponseMsg> deleteTask(@RequestBody TaskRequest taskRequest) {
 
 		BasicResponseMsg response = new BasicResponseMsg();
